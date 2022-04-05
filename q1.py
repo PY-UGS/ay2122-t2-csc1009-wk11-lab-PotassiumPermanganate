ec84a0ec07992931548e4879564a3714fe8e0766
PotassiumPermanganate <90330184+PotassiumPermanganate@users.noreply.github.com>
2022-04-05 09:56:57 +0800

    Add files via upload

--- q1.py
+++ q1.py
@@ -4,22 +4,28 @@ class Calculator:
        
 
     def __init__(self, x, y):
+        #initialise
         self.x = x
         self.y = y
     
     def adder(self):
+        #Add
         return self.x + self.y
 
     def subtractor(self):
+        # minus
         return self.x - self.y
 
     def multiplier(self):
+        # multiplier
         return self.x * self.y
     
     def divider(self):
+        #divider
         return self.x / self.y
 
     def clear(self):
+        # clear the value
         self.x = 0
         self.y = 0
 
@@ -27,6 +33,7 @@ class Calculator:
 def main():
     user_input_one  = input('one: ')
     user_input_two  = input('two: ')
+    # Calculate
     calculator = Calculator(int(user_input_one), int(user_input_two))
     
     print(calculator.adder())
--- q2.py
+++ q2.py
@@ -4,32 +4,40 @@ class ClockTime:
 
 
     def setHours(self, hour):
-
+        # Get hour
         self.hour = hour
     
     def setMinutes(self, minute):
+        # Get min
         self.minute = minute
 
     def setSecond(self, second):
+        # Get second
         self.second = second
 
     def setTime(self, hour, minute, second):
+        # Get time with the above ans
         self.setHours(hour)
         self.setMinutes(minute)
         self.setSecond(second)
 
     def showTime(self):
+        # output
         print(f'{self.hour}:{self.minute}:{self.second}')
 
 
 
 def main():
+    # init the clock
     clockTime = ClockTime()
     try:
+        # input the time and then convert to number 
+        # catch error
         hour = int(input('hour: '))
         minute = int(input('minute: '))
         second = int(input('second: '))
 
+        # check limit if within range
         if hour >= 0 and hour <= 24 and minute <= 59 and minute >=0 and second >= 0 and second <= 59:
             clockTime.setTime(hour, minute, second) 
             clockTime.showTime()
